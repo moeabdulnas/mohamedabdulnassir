32ebfee5eacd99a0aee37e068b8e8c7e78f6263d
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-08 19:37:06 +0200

    Initial design

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import img from "./img.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1 id="m">m</h1>
+        <section className="links">
+        </section>
+        <div className="container">
+          <div className="containerText">
+            <h1 id="greeting">Hello 👋</h1>
+            <h1 id="name">I'm Mohamed Abdulnassir</h1>
+            <h2>Software developer</h2>
+          </div>
+          <img src={img} alt="profile"></img>
+        </div>
+      </div>
     </div>
   );
 }
