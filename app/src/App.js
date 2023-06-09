2fafe4d5ac3f835d0cb559bce859df2a055b6d78
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-09 11:36:33 +0200

    Styling, sizing. Added links

--- app/src/App.js
+++ app/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <div className="header">
         <h1 id="m">m</h1>
         <div className="links">
-          {/* <h1 id="linkedin">in</h1> 
-           <h1 id="github">g</h1> */}
-          <i class="devicon-linkedin-plain header-logo"></i>
-          <i class="devicon-github-original header-logo"></i>
+          <a href="https://www.linkedin.com/in/mohamedabdulnassir/">
+            <i class="devicon-linkedin-plain header-icon"></i>
+          </a>
+          <a href="https://github.com/moeabdulnas">
+            <i class="devicon-github-original header-icon"></i>
+          </a>
         </div>
       </div>
-
         <div className="container">
-
           <div className="containerText">
             <h1 id="greeting">Hello <span id="wave">👋</span></h1>
             <h1 id="name">I'm Mohamed Abdulnassir</h1>
@@ -27,44 +27,43 @@ function App() {
               <h1 id="techText">Technologies I mainly use</h1>
               <div className="stack"> 
                 <div className="javascript tech">
-                  <i class="devicon-javascript-plain colored"></i>
+                  <i class="devicon-javascript-plain colored tech-icon"></i>
                   <p id="tech-name">javascript</p>
                 </div>           
                 <div className="python tech">
-                  <i class="devicon-python-plain colored"></i>
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-python-plain colored tech-icon"></i>
+                  <p id="tech-name">python</p>
                 </div>           
                 <div className="react tech">
-                  <i class="devicon-react-original colored"></i>
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-react-original colored tech-icon"></i>
+                  <p id="tech-name">react</p>
                 </div>           
                 <div className="git tech">
-                  <i class="devicon-git-plain colored"></i> 
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-git-plain colored tech-icon"></i> 
+                  <p id="tech-name">git</p>
                 </div>           
                 <div className="node tech">
-                  <i class="devicon-nodejs-plain colored"></i>
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-nodejs-plain colored tech-icon"></i>
+                  <p id="tech-name">node.js</p>
                 </div>           
                 <div className="express tech">
-                  <i class="devicon-express-original"></i>
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-express-original tech-icon"></i>
+                  <p id="tech-name">express</p>
                 </div>           
                 <div className="mongodb tech">
-                  <i class="devicon-mongodb-plain colored"></i>
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-mongodb-plain colored tech-icon"></i>
+                  <p id="tech-name">mongodb</p>
                 </div>           
                 <div className="html tech">
-                  <i class="devicon-html5-plain colored"></i>
-                  <p id="tech-name">javascript</p>
+                  <i class="devicon-html5-plain colored tech-icon"></i>
+                  <p id="tech-name">html</p>
                 </div>           
-                  <i class="devicon-css3-plain colored"></i>
-                  <p id="tech-name">javascript</p>
                 <div className="css tech">
+                  <i class="devicon-css3-plain colored tech-icon"></i>
+                  <p id="tech-name">css</p>
                 </div>           
               </div>
             </div>
-
           </div>
             <img src={img} alt="profile"></img>
         </div>
