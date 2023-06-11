48570d9152421a5d9356522c5a1552230817e2bf
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-11 21:44:00 +0200

    Keep restyling for better responsiveness and maintenance

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -16,65 +16,66 @@ function App() {
           </a>
         </div>
       </div>
-        <div className="container">
-          <div className="about">
-            <div className="aboutText">
-              <h1 id="greeting">Hello <span id="wave">👋</span></h1>
-              <h1 id="name">I'm Mohamed Abdulnassir</h1>
-              <h3>Software developer</h3>
-              <h5>Linköping, Sweden</h5>
-            </div>
-            <div className="imageContainer">
-              <img src={img} alt="profile"></img>
-            </div>
-          </div>
 
-            <div className="techContainer">
-              <h1 id="techText">Technologies I mainly use</h1>
-              <div className="stack"> 
-                <div className="javascript tech">
-                  <i class="devicon-javascript-plain colored tech-icon"></i>
-                  <p id="tech-name">javascript</p>
-                </div>           
-                <div className="python tech">
-                  <i class="devicon-python-plain colored tech-icon"></i>
-                  <p id="tech-name">python</p>
-                </div>           
-                <div className="react tech">
-                  <i class="devicon-react-original colored tech-icon"></i>
-                  <p id="tech-name">react</p>
-                </div>           
-                <div className="git tech">
-                  <i class="devicon-git-plain colored tech-icon"></i> 
-                  <p id="tech-name">git</p>
-                </div>           
-                <div className="node tech">
-                  <i class="devicon-nodejs-plain colored tech-icon"></i>
-                  <p id="tech-name">node.js</p>
-                </div>           
-                <div className="express tech">
-                  <i class="devicon-express-original tech-icon"></i>
-                  <p id="tech-name">express</p>
-                </div>           
-                <div className="mongodb tech">
-                  <i class="devicon-mongodb-plain colored tech-icon"></i>
-                  <p id="tech-name">mongodb</p>
-                </div>           
-                <div className="html tech">
-                  <i class="devicon-html5-plain colored tech-icon"></i>
-                  <p id="tech-name">html</p>
-                </div>           
-                <div className="css tech">
-                  <i class="devicon-css3-plain colored tech-icon"></i>
-                  <p id="tech-name">css</p>
-                </div>           
-              </div>
-            </div>
+      <div className="container">
+        <div className="about">
+          <div className="aboutText">
+            <h1 id="greeting">Hello <span id="wave">👋</span></h1>
+            <h1 id="name">I'm Mohamed Abdulnassir</h1>
+            <h3>Software developer</h3>
+            <h5>Linköping, Sweden</h5>
+          </div>
+          <div className="imageContainer">
+            <img src={img} alt="profile"></img>
+          </div>
         </div>
+
+        <div className="techContainer">
+          <h1 id="techText">Technologies I mainly use</h1>
+          <div className="stack"> 
+            <div className="javascript tech">
+              <i class="devicon-javascript-plain colored tech-icon"></i>
+              <p id="tech-name">javascript</p>
+            </div>           
+            <div className="python tech">
+              <i class="devicon-python-plain colored tech-icon"></i>
+              <p id="tech-name">python</p>
+            </div>           
+            <div className="react tech">
+              <i class="devicon-react-original colored tech-icon"></i>
+              <p id="tech-name">react</p>
+            </div>           
+            <div className="git tech">
+              <i class="devicon-git-plain colored tech-icon"></i> 
+              <p id="tech-name">git</p>
+            </div>           
+            <div className="node tech">
+              <i class="devicon-nodejs-plain colored tech-icon"></i>
+              <p id="tech-name">node.js</p>
+            </div>           
+            <div className="express tech">
+              <i class="devicon-express-original tech-icon"></i>
+              <p id="tech-name">express</p>
+            </div>           
+            <div className="mongodb tech">
+              <i class="devicon-mongodb-plain colored tech-icon"></i>
+              <p id="tech-name">mongodb</p>
+            </div>           
+            <div className="html tech">
+              <i class="devicon-html5-plain colored tech-icon"></i>
+              <p id="tech-name">html</p>
+            </div>           
+            <div className="css tech">
+              <i class="devicon-css3-plain colored tech-icon"></i>
+              <p id="tech-name">css</p>
+            </div>           
+          </div>
+        </div>
+      </div>
         {/*Container ends here */}
-        <div id="buttonContainer">
-          <button id="button">Get in touch</button>
-        </div>
+      <div id="buttonContainer">
+        <button id="button">Get in touch</button>
+      </div>
     </div>
   );
 }
