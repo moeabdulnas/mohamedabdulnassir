f87bf44aef7056c14ede194e3ea5ee59bec7b01b
Mohamed Abdulnassir <mohab918@student.liu.se>
2023-06-13 14:58:32 +0200

    Contact html and layout

--- app/src/Contact.jsx
+++ app/src/Contact.jsx
@@ -1,9 +1,25 @@
-
+import "./Contact.css"
 
 const Contact = (props) => {
     return (
         <div className="contact">
-            
+            <h1>Get in touch</h1>
+            <div className="contactContainer">
+                <form action="" className="form">
+                    <div className="nameDiv">
+                        <label htmlFor="name">Your name:</label>
+                        <input type="text" id="name"/>
+                    </div>
+                    <div className="emailDiv">
+                        <label htmlFor="email">Your email:</label>
+                        <input type="email" id="email"/>
+                    </div>
+                    <div className="messageDiv">
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="message" id="message" rows="10"></textarea>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
